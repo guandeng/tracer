5d36034196b55e0423d1511de0fc16d994c855c2
guandeng <guandeng@hotmail.com>
2022-08-04 22:13:47 +0800

    优化自动上报事件

--- src/Listeners/RpcListener.php
+++ src/Listeners/RpcListener.php
@@ -10,62 +10,66 @@ use Illuminate\Http\Request;
 
 use const OpenTracing\Formats\TEXT_MAP;
 
-class RpcListener extends Context
+class RpcListener
 {
     use SpanStarter;
 
     private $request;
+    private $span;
+    private $tracer;
+    private $path = null;
 
-    public function __construct()
+    public function __construct($path = null)
     {
         $this->spanTagManager = new spanTagManager();
         $this->request = app(Request::class);
+        $this->path = $path;
+        if ($path == null) {
+            $this->path = $this->request->getPathInfo();
+        }
+        $this->initTracer();
     }
 
-    public function onJobProcessing($path = null)
+    public function onJobProcessing()
     {
-        if ($path == null) {
-            $path = $this->request->getPathInfo();
-        }
-        $name = config('opentracing.default');
-        $this->tracer = resolve('tracer')->make($name);
-        $key = "JsonRPC send [{$path}]";
-        $span = $this->startSpan($key);
         $headers = [];
-        $this->tracer->inject($span->getContext(), TEXT_MAP, $headers);
+        $this->tracer->inject($this->span->getContext(), TEXT_MAP, $headers);
         foreach ($headers as $header => $value) {
             $this->request->headers->set($header, $value);
         }
-        $span->setTag($this->spanTagManager->get('rpc', 'path'), $path);
-        static::$tracers = $this->tracer;
-        static::$span = $span;
+        $this->span->setTag($this->spanTagManager->get('rpc', 'path'), $this->path);
         return $headers;
     }
 
     public function onJobProcessed($result = true)
     {
-        $tracers = static::$tracers;
-        $span = static::$span;
-        $span->setTag($this->spanTagManager->get('rpc', 'status'), $result ? 'OK' : 'Failed');
-        $span->finish();
-        $tracers->flush();
+        $this->span->setTag($this->spanTagManager->get('rpc', 'status'), $result ? 'OK' : 'Failed');
+        $this->span->finish();
+        $this->tracer->flush();
     }
 
     public function onJobProcessException(\Throwable $exception)
     {
-        $tracers = static::$tracers;
-        $span = static::$span;
-        $this->appendExceptionToSpan($span, $exception);
-        $span->finish();
-        $tracers->flush();
+        $this->appendExceptionToSpan($exception);
+        $this->span->finish();
+        $this->tracer->flush();
     }
 
-    protected function appendExceptionToSpan($span, \Throwable $exception): void
+    protected function appendExceptionToSpan(\Throwable $exception): void
     {
-        $span->setTag('exception', true);
-        $span->setTag($this->spanTagManager->get('exception', 'class'), get_class($exception));
-        $span->setTag($this->spanTagManager->get('exception', 'code'), $exception->getCode());
-        $span->setTag($this->spanTagManager->get('exception', 'message'), $exception->getMessage());
-        $span->setTag($this->spanTagManager->get('exception', 'stack_trace'), (string) $exception);
+        $this->span->setTag('exception', true);
+        $this->span->setTag($this->spanTagManager->get('exception', 'class'), get_class($exception));
+        $this->span->setTag($this->spanTagManager->get('exception', 'code'), $exception->getCode());
+        $this->span->setTag($this->spanTagManager->get('exception', 'message'), $exception->getMessage());
+        $this->span->setTag($this->spanTagManager->get('exception', 'stack_trace'), (string) $exception);
     }
+    
+    protected function initTracer()
+    {
+        $name = config('opentracing.default');
+        $this->tracer = resolve('tracer')->make($name);
+        $key = "JsonRPC send [{$this->path}]";
+        $this->span = $this->startSpan($key);
+    }
+
 }
